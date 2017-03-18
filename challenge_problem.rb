aed5f3d61397100de3533763e2baf0301326d537
Stephanie Barker <song_mage@yahoo.com>
2017-03-18 18:14:21 -0400

    needs major refactoring but now works as intended

--- challenge_problem.rb
+++ challenge_problem.rb
@@ -14,37 +14,31 @@ def word_position(word)
   section_size = max_position
 
   possibilities = factorial(length_left)/less_factorial(duplicates)
-  puts possibilities
 
   # atat should return 2
 
   word.each_char do |char|
-    section = letter_order.index(char) + 1
+    section = which_section(duplicates, char)
+
     duplicates[char] -= 1
 
-    if duplicates[char] == 0
-        letter_order -= [char]
-    end
-
-    # ttaa = 6
-    # min = 4
+    # zzbba = 30
+    # min = 19
 
     possibilities = factorial(length_left - 1)/less_factorial(duplicates)
-    puts possibilities
 
-    min_position += (section_size / length_left) * (section - 1)
+    if duplicates[char] == 0
+      letter_order -= [char]
+    end
+
+
+    min_position += section_size / length_left.to_f * (section)
     max_position = (min_position - 1) + possibilities 
 
-    # min = 1, max = 3, length_left
-    #division_factor = length_left / how_many_duplicates(duplicates)
-
-    #min_position += (section_size / division_factor) * (section - 1)
-    #max_position = (min_position - 1) + section_size / division_factor
-
-    puts "#{min_position}, #{max_position}"
+    # puts "#{min_position}, #{max_position}"
 
     if max_position == min_position
-      return min_position
+      return min_position.to_i
     else
       length_left -= 1
       section_size = possibilities
@@ -71,23 +65,12 @@ def less_factorial(someHash)
   total
 end
 
-def how_many_duplicates(someHash)
-  dupes = 1
+def which_section(someHash, character)
+  sum = 0
   someHash.each do |char, count|
-    if count > 1
-      dupes += (count - 1)
+    if character == char
+      return sum
     end
+    sum += count
   end
-  dupes
 end
-
-puts "babc: #{word_position('babc')}" # 4
-puts "atat: #{word_position('atat')}" # 2
-puts "aatt: #{word_position('aatt')}" # 1
-puts "ttaa: #{word_position('ttaa')}" # 6
-puts "zzcba: #{word_position('zzcba')}" # 60
-puts "zyxwv: #{word_position('zyxwv')}" # 120
-puts "zzbba: #{word_position('zzbba')}" # 30
-puts "zzzba: #{word_position('zzzba')}" # 20
-puts "zbzaz: #{word_position('zbzaz')}" # 13
-puts "zzzzcba #{word_position('zzzzcba')}" # 210
