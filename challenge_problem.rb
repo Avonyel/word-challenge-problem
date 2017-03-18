5d1519fcc7dfd1ec57ca00dde10fd2efd2578965
Stephanie Barker <song_mage@yahoo.com>
2017-03-18 13:43:21 -0400

    starting to fix cases with duplicate letters

--- challenge_problem.rb
+++ challenge_problem.rb
@@ -10,6 +10,13 @@ def word_position(word)
   max_position = factorial(word.length)
   less_positions = 1
 
+  duplicates.each do |char, count|
+    if count > 1
+      less_positions *= factorial(count)
+    end
+  end
+
+  max_position /= less_positions
   divide_by = word.length
   section_size = max_position
 
@@ -18,11 +25,13 @@ def word_position(word)
     duplicates[char] -= 1
 
     if duplicates[char] == 0
-      letter_order -= [char]
+        letter_order -= [char]
     end
 
+    # need to get max of 60
+    # section = 4, divide_by = 5, section_size = 60
     min_position += section_size / divide_by * (section - 1)
-    max_position = (min_position - 1) + (section_size / divide_by)
+    max_position = (min_position - 1) + ((section_size / divide_by) * (duplicates[char] + 1))
 
     if max_position == min_position
       return min_position
@@ -41,3 +50,5 @@ def factorial(number)
   end
   total
 end
+
+puts word_position("zabc")
